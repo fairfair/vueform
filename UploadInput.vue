c893c39e30ffce4ac6428f73f90e04a196d73983
eloiletagant <anatole.gilet@protonmail.com>
2021-10-30 22:36:49 +0200

    Finish Upload component

--- UploadInput.vue
+++ UploadInput.vue
@@ -1,81 +1,86 @@
 <template>
-  <div>
     <div class="mt-2">
       <div 
-        v-if="imgs.length < 1" 
         class="flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
         @drop.prevent="dropFile"
       >
         <div class="space-y-1 text-center">
-          <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-          <div class="flex text-sm text-gray-600">
-            <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-green-600 hover:text-green-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-green-500">
-              <span v-if="multiple">Ajouter des documents</span>
-              <span v-else>Ajouter un document</span>
-              <input id="file-upload" name="file-upload" type="file" class="sr-only" @change="previewFilesEvent" :multiple="multiple"/>
-            </label>
-            <p class="pl-1">ou glisser-déposer ici</p>
-          </div>
-          <p class="text-xs text-gray-500">
-            Image (formats PNG, JPG) ou document (format PDF)
-          </p>
+          <ArrowCircleUpIcon class="mx-auto h-12 w-12 text-gray-400"></ArrowCircleUpIcon>
+      <div class="flex text-lg text-center">
+        <label for="file-upload" class="w-full cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+          <span>Sélectionnez</span>
+          <input id="file-upload" name="file-upload" type="file" class="sr-only" @change="uploadFile"/>
+          <span class="pl-1 text-gray-600">ou glissez-déposez votre {{ label}}</span>
+        </label>
+      </div>
+      <p class="text-sm text-gray-400 italic">{{ description }}</p>
         </div>
       </div>
-      <div v-else class="flex justify-center items-center">
-        <div class="inline-block relative w-full" v-for="img in imgs" :key="img.id">
-          <embed :src="img" class="shadow-md w-full "/>
+      <div v-if="loadedFile" class="flex justify-center items-center my-3">
+        <div class="inline-block relative w-full">
+          <embed :src="loadedFile" class="shadow-md w-full" type="application/pdf" width="100%" height="500px"/>
           <button @click="deletePreview" class="absolute top-2 right-2 p-2 bg-white bg-opacity-70 hover:bg-gray-200 rounded-lg text-gray-600">
             <XIcon class="w-6 h-6"/>
           </button>
         </div>
       </div>
+
+      <div v-if="existingFile || loadedFile">
+        <button @click="openFile" class="btn-primary w-full my-3">
+          Voir le fichier
+           <EyeIcon class="w-6 h-6 mx-3"></EyeIcon> 
+          </button>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
 import { ref } from 'vue';
-import { XIcon } from '@heroicons/vue/outline';
+import { XIcon, ArrowCircleUpIcon, EyeIcon } from '@heroicons/vue/outline';
 
 export default {
   props: {
-    multiple: Boolean,
-    content: {
-      default: null,
-    },
+    existingFile: String,
+    label: String,
+    description: String,
   },
   components: {
-      XIcon,
+      XIcon, ArrowCircleUpIcon, EyeIcon
   },
-  setup(props, { emit }) {
-    const imgs = ref([]);
 
-    const previewImage = ref(props.content);
+  setup(props, { emit }) {
+    let loadedFile = ref();
+
     const getContent = (img) => img.split(',')[1];
 
     const getContentType = (img) => img.split(';')[0].split(':')[1];
 
-    const previewFilesEvent = (event) => setPreviewFromFile(event.target.files);
+    const deletePreview = () => loadedFile.value = null;
 
-    const deletePreview = () => imgs.value = [];
+    const uploadFile = (event) => convertFileAsUrl(event.target.files);
 
-    const dropFile = (event) => setPreviewFromFile(event.dataTransfer.files);
+    const dropFile = (event) => convertFileAsUrl(event.dataTransfer.files);
 
-    const setPreviewFromFile = (files) => {
+    const openFile = () => {
+      const value = loadedFile.value !== undefined ? loadedFile.value : `data:application/pdf;base64,${props.existingFile}`;
+      console.log(value)
+
+      const tab = window.open('', '');
+      const content = (`<embed width=100% height=100%  src="${value}" type="application/pdf" />`);
+      tab.document.write(content);
+    };
+
+    const convertFileAsUrl = (files) => {
       Array.from(files).forEach((_, index) => {
         if (files && files[index]) {
           let reader = new FileReader
           reader.onload = e => {
-            previewImage.value = e.target.result;
-            imgs.value.push(previewImage.value);
-            if (!props.multiple) {
-              emit('update', {
-                content: getContent(imgs.value[0]),
-                "content-type": getContentType(imgs.value[0]),
-              });
-            }
+            loadedFile.value = e.target.result;
+
+            emit('update', {
+              content: getContent(loadedFile.value),
+              "content-type": getContentType(loadedFile.value),
+            });
           }
           reader.readAsDataURL(files[index])
         }
@@ -84,9 +89,10 @@ export default {
 
 
     return {
-      imgs,
-      previewFilesEvent,
+      loadedFile,
+      uploadFile,
       deletePreview,
+      openFile,
       dropFile
     }
   }
