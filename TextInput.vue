d058f1bc093844686df1496634cc29140222551a
eloiletagant <anatole.gilet@protonmail.com>
2022-01-03 11:23:17 +0100

    Add textarea type in TextInput

--- TextInput.vue
+++ TextInput.vue
@@ -7,7 +7,10 @@
         <span class="text-sm text-gray-500">{{ description}}</span>
       </dt>
       <dd class="flex text-sm text-gray-900" :class="labelTop ? 'col-span-3' : 'col-span-2'">
-        <div class="flex-grow">
+        <div class="flex-grow" v-if="type === 'textarea'">
+          <textarea v-model="value" :type="type" :name="field" @keyup.enter="submit(field, value)" class="block w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm" />
+        </div>
+        <div class="flex-grow" v-else>
           <input v-model="value" :type="type" :name="field" @keyup.enter="submit(field, value)" class="block w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm" />
         </div>
         <div class="mx-5 flex-shrink-0 my-auto" v-if="submitButton">
